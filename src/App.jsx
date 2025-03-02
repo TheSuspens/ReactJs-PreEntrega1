26c9f711293e8c5ff78db3476cfb4e8ab453fd61
TheSuspens <alvaro_setores_07@hotmail.com>
2025-03-02 18:18:46 -0300

    primera entrega alvaro

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import NavBar from './components/navbar';
-import ItemListContainer from './components/itemListContainer';
+import NavBar from '../src/components/navbar';
+import ItemListContainer from '../src/components/itemListContainer';
 
 const App = () => {
   const mensajeBienvenida = "Bienvenido a nuestra tienda";
